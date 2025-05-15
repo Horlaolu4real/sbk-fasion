5593544a86ce24b72105896159a011372fae474d
Horlaolu4real <olaoluwayusuf121@gmail.com>
2025-05-15 12:49:57 +0100

    add: add some design

--- src/feature/prod/component/navbar/index.tsx
+++ src/feature/prod/component/navbar/index.tsx
@@ -7,18 +7,20 @@ import {
   LoveIcon,
   SearchIcon,
 } from "@/assest/icon";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import frames from "@/assest/image/Frame 1686562473.svg";
+import { FaTimes } from "react-icons/fa";
 
 import styles from "./styles.module.scss";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
     <div className={styles.content}>
       <div className={styles.Navbar}>
-        <div className={styles.hamburg}>
+        <div className={styles.hamburg} onClick={() => setShowMenu(true)}>
           <HamburgerIcon />
         </div>
         <div className={styles.link}>
@@ -70,6 +72,39 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      {showMenu && (
+        <div className={styles.mobileMenu}>
+          <div className={styles.closeIcon} onClick={() => setShowMenu(false)}>
+            <FaTimes />
+          </div>
+
+          <ul className={styles.mobileList}>
+            <li>
+              <Link href={"/home"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/categories"}>Categories</Link>
+            </li>
+            <li>
+              <Link href={"/featured Products"}>Featured Products</Link>
+            </li>
+            <li>
+              <Link href={"/billing"}>Contact Us</Link>
+            </li>
+          </ul>
+
+          <div className={styles.mobileInputField}>
+            <input
+              type="text"
+              placeholder="What are you looking for ?"
+              className={styles.field}
+            />
+            <div className={styles.searchIcon}>
+              <SearchIcon />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
